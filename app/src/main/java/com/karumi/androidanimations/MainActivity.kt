4da8d1a5a0c90ba4272f4aab76c86b5d8789c175
Serchinastico <54cymru@gmail.com>
2019-04-04 17:44:04 +0200

    Configure everything with the navigation library

--- app/src/main/java/com/karumi/androidanimations/MainActivity.kt
+++ app/src/main/java/com/karumi/androidanimations/MainActivity.kt
@@ -1,60 +1,22 @@
 package com.karumi.androidanimations
 
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.afollestad.recyclical.ViewHolder
-import com.afollestad.recyclical.datasource.dataSourceOf
-import com.afollestad.recyclical.setup
-import com.afollestad.recyclical.withItem
-import com.karumi.androidanimations.extensions.exhaustive
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        configureSections()
+        configureToolbar()
     }
 
-    private fun configureSections() {
-        val layoutManager = LinearLayoutManager(this)
-        val dataSource = dataSourceOf(*Section.values())
-
-        allSections.setup {
-            withLayoutManager(layoutManager)
-            withDataSource(dataSource)
-
-            withItem<Section>(R.layout.view_section) {
-                onBind(::SectionViewHolder) { _, item ->
-                    name.text = when (item) {
-                        Section.ViewAnimator -> R.string.section_view_animator_framework
-                        Section.PropertyAnimation -> R.string.section_property_animation_framework
-                        Section.AnimatedVector -> R.string.section_animated_vector_drawable
-                    }.resolve()
-                }
-
-                onClick { _, item ->
-                    when (item) {
-                        Section.ViewAnimator -> R.string.section_view_animator_framework
-                        Section.PropertyAnimation -> R.string.section_property_animation_framework
-                        Section.AnimatedVector -> R.string.section_animated_vector_drawable
-                    }.exhaustive
-                }
-            }
-        }
-    }
-
-    private fun Int.resolve(): String = getString(this)
-
-    private enum class Section {
-        ViewAnimator, PropertyAnimation, AnimatedVector
-    }
-
-    private class SectionViewHolder(itemView: View) : ViewHolder(itemView) {
-        val name: TextView = itemView.findViewById(R.id.sectionName)
+    private fun configureToolbar() {
+        val navController = findNavController(R.id.hostFragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
---
+++ app/src/main/java/com/karumi/androidanimations/MainFragment.kt
@@ -0,0 +1,67 @@
+package com.karumi.androidanimations
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.afollestad.recyclical.ViewHolder
+import com.afollestad.recyclical.datasource.dataSourceOf
+import com.afollestad.recyclical.setup
+import com.afollestad.recyclical.withItem
+import com.karumi.androidanimations.extensions.exhaustive
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_main, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configureSections()
+    }
+
+    private fun configureSections() {
+        val layoutManager = LinearLayoutManager(requireContext())
+        val dataSource = dataSourceOf(*Section.values())
+
+        allSections.setup {
+            withLayoutManager(layoutManager)
+            withDataSource(dataSource)
+
+            withItem<Section>(R.layout.view_section) {
+                onBind(::SectionViewHolder) { _, item ->
+                    name.text = when (item) {
+                        Section.ViewAnimator -> R.string.section_view_animator_framework
+                        Section.PropertyAnimation -> R.string.section_property_animation_framework
+                        Section.AnimatedVector -> R.string.section_animated_vector_drawable
+                    }.resolve()
+                }
+
+                onClick { _, item ->
+                    when (item) {
+                        Section.ViewAnimator -> R.string.section_view_animator_framework
+                        Section.PropertyAnimation -> R.string.section_property_animation_framework
+                        Section.AnimatedVector -> R.string.section_animated_vector_drawable
+                    }.exhaustive
+                }
+            }
+        }
+    }
+
+    private fun Int.resolve(): String = getString(this)
+
+    private enum class Section {
+        ViewAnimator, PropertyAnimation, AnimatedVector
+    }
+
+    private class SectionViewHolder(itemView: View) : ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.sectionName)
+    }
+}
