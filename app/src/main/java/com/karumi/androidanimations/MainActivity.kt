93c9c18f71c82752f996b9ba46d73b1ca9561ff9
Serchinastico <54cymru@gmail.com>
2019-04-04 17:31:02 +0200

    Add a list of 3 elements to show all the sections of the training

--- app/src/main/java/com/karumi/androidanimations/MainActivity.kt
+++ app/src/main/java/com/karumi/androidanimations/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.karumi.androidanimations
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.afollestad.recyclical.ViewHolder
+import com.afollestad.recyclical.datasource.dataSourceOf
+import com.afollestad.recyclical.setup
+import com.afollestad.recyclical.withItem
+import com.karumi.androidanimations.extensions.exhaustive
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configureSections()
+    }
+
+    private fun configureSections() {
+        val layoutManager = LinearLayoutManager(this)
+        val dataSource = dataSourceOf(*Section.values())
+
+        allSections.setup {
+            withLayoutManager(layoutManager)
+            withDataSource(dataSource)
+
+            withItem<Section>(R.layout.view_section) {
+                onBind(::SectionViewHolder) { _, item ->
+                    name.text = when (item) {
+                        Section.ViewAnimator -> R.string.section_view_animator_framework
+                        Section.PropertyAnimation -> R.string.section_property_animation_framework
+                        Section.AnimatedVector -> R.string.section_animated_vector_drawable
+                    }.resolve()
+                }
+
+                onClick { _, item ->
+                    when (item) {
+                        Section.ViewAnimator -> R.string.section_view_animator_framework
+                        Section.PropertyAnimation -> R.string.section_property_animation_framework
+                        Section.AnimatedVector -> R.string.section_animated_vector_drawable
+                    }.exhaustive
+                }
+            }
+        }
+    }
+
+    private fun Int.resolve(): String = getString(this)
+
+    private enum class Section {
+        ViewAnimator, PropertyAnimation, AnimatedVector
+    }
+
+    private class SectionViewHolder(itemView: View) : ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.sectionName)
     }
 }
---
+++ app/src/main/java/com/karumi/androidanimations/extensions/exhaustive.kt
@@ -0,0 +1,3 @@
+package com.karumi.androidanimations.extensions
+
+val Any?.exhaustive: Unit get() = Unit
