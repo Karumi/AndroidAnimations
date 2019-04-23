471ca8e7baac6ee26c2cd66888f07527561d7905
Serchinastico <54cymru@gmail.com>
2019-04-23 16:07:49 +0200

    Implement vectorized and keyframe animations

--- app/src/main/java/com/karumi/androidanimations/drawablegraphicsanimation/DrawableGraphicsSimpleAnimation.kt
+++ app/src/main/java/com/karumi/androidanimations/drawablegraphicsanimation/DrawableGraphicsSimpleAnimation.kt
@@ -1,14 +1,21 @@
 package com.karumi.androidanimations.drawablegraphicsanimation
 
 import android.content.Context
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.karumi.androidanimations.R
 import com.karumi.androidanimations.drawablegraphicsanimation.DrawableGraphicsAnimationFragment.DrawableGraphicsAnimation
+import com.karumi.androidanimations.extensions.exhaustive
 
 interface DrawableGraphicsSimpleAnimation {
     class VH(itemView: View) : com.afollestad.recyclical.ViewHolder(itemView) {
         val name: TextView = itemView.findViewById(R.id.animationName)
+        val animationView: AppCompatImageView = itemView.findViewById(R.id.animationView)
     }
 
     class Binder(val getContext: () -> Context) {
@@ -19,7 +26,7 @@ interface DrawableGraphicsSimpleAnimation {
 
         private fun VH.bind(item: DrawableGraphicsAnimation) {
             name.text = getAnimationName(item)
-            animate(item, name)
+            animate(item, animationView)
         }
 
         private fun getAnimationName(item: DrawableGraphicsAnimation): String = when (item) {
@@ -28,12 +35,33 @@ interface DrawableGraphicsSimpleAnimation {
             DrawableGraphicsAnimation.Lottie -> R.string.drawable_graphics_animation_with_lottie
         }.let { getContext().getString(it) }
 
-        private fun animate(item: DrawableGraphicsAnimation, view: View) {
+        private fun animate(item: DrawableGraphicsAnimation, view: AppCompatImageView) {
             when (item) {
-                DrawableGraphicsAnimation.Keyframe -> R.string.drawable_graphics_animation_by_keyframe
-                DrawableGraphicsAnimation.Vector -> R.string.drawable_graphics_animation_with_vector
-                DrawableGraphicsAnimation.Lottie -> R.string.drawable_graphics_animation_with_lottie
-            }
+                DrawableGraphicsAnimation.Keyframe -> {
+                    view.setBackgroundResource(R.drawable.keyframe_animation)
+                    (view.background as AnimationDrawable).start()
+                }
+                DrawableGraphicsAnimation.Vector -> {
+                    val animation = AnimatedVectorDrawableCompat.create(
+                        getContext(),
+                        R.drawable.vectorized_animation
+                    )?.apply {
+                        repeatAnimation()
+                        start()
+                    }
+                    view.setImageDrawable(animation)
+                }
+                DrawableGraphicsAnimation.Lottie -> {
+                }
+            }.exhaustive
+        }
+
+        private fun AnimatedVectorDrawableCompat.repeatAnimation() {
+            registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
+                override fun onAnimationEnd(drawable: Drawable?) {
+                    start()
+                }
+            })
         }
     }
 }
