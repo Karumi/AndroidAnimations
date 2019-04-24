38f2c88e34456e7c39293fee6f79837966c930e0
Serchinastico <54cymru@gmail.com>
2019-04-24 16:45:39 +0200

    Replace the seekbar by two buttons to play and stop animations

--- app/src/main/java/com/karumi/androidanimations/drawablegraphicsanimation/DrawableGraphicsExerciseAnimation.kt
+++ app/src/main/java/com/karumi/androidanimations/drawablegraphicsanimation/DrawableGraphicsExerciseAnimation.kt
@@ -2,8 +2,8 @@ package com.karumi.androidanimations.drawablegraphicsanimation
 
 import android.content.Context
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
-import android.widget.SeekBar
 import android.widget.TextView
 import com.karumi.androidanimations.R
 
@@ -13,36 +13,25 @@ interface DrawableGraphicsExerciseAnimation {
         val keyframeAnimationView: ImageView = itemView.findViewById(R.id.keyframeAnimationView)
         val vectorizedAnimationView: ImageView = itemView.findViewById(R.id.vectorizedAnimationView)
         val lottieAnimationView: ImageView = itemView.findViewById(R.id.lottieAnimationView)
-        val seekBar: SeekBar = itemView.findViewById(R.id.seekBar)
+        val playButton: Button = itemView.findViewById(R.id.playButton)
+        val stopButton: Button = itemView.findViewById(R.id.stopButton)
     }
 
     class Binder(val getContext: () -> Context) {
         operator fun invoke(receiver: VH) = receiver.bind()
 
         private fun VH.bind() {
-            name.text =
-                R.string.drawable_graphics_animation_exercise.let { getContext().getString(it) }
+            name.text = R.string.drawable_graphics_animation_exercise
+                .let { getContext().getString(it) }
 
-            seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(
-                    seekBar: SeekBar?,
-                    progress: Int,
-                    fromUser: Boolean
-                ) {
-                    TODO(
-                        """
-                        | Render the animation that can be controlled with this SeekBar.
-                        | You have the same animation in all formats: keyframe, vectorized and
-                        | lottie. Configure all of them.
-                        | """.trimMargin()
-                    )
-                }
-
-                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
-                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-
-            })
+            TODO(
+                """
+                | Render the animation that can be controlled with two buttons, one to play
+                | and one to stop.
+                | You have the same animation in all formats: keyframe, vectorized and
+                | lottie. Configure all of them one by one.
+                | """.trimMargin()
+            )
         }
     }
 }
