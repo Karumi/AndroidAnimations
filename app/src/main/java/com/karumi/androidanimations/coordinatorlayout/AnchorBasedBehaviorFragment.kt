9f8d6e4b2e8c833c7ec10f29550d429a65df90c0
Serchinastico <54cymru@gmail.com>
2019-05-16 18:15:44 +0200

    Use regular scroll behavior depending on the fragment I'm in, in this case using the Anchor based but we will eventually move it to a different example

--- app/src/main/java/com/karumi/androidanimations/coordinatorlayout/AnchorBasedBehaviorFragment.kt
+++ app/src/main/java/com/karumi/androidanimations/coordinatorlayout/AnchorBasedBehaviorFragment.kt
@@ -4,19 +4,65 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.afollestad.recyclical.datasource.dataSourceOf
+import com.afollestad.recyclical.setup
+import com.afollestad.recyclical.withItem
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS
+import com.google.android.material.appbar.AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL
+import com.google.android.material.appbar.AppBarLayout.LayoutParams.SCROLL_FLAG_SNAP
 import com.karumi.androidanimations.R
 import com.karumi.androidanimations.base.BaseFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_anchor_based_behavior.*
 
 class AnchorBasedBehaviorFragment : BaseFragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_anchor_based_behavior, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_anchor_based_behavior, container, false).also {
+        setToolbarScrollFlags(SCROLL_FLAG_SCROLL or SCROLL_FLAG_SNAP or SCROLL_FLAG_ENTER_ALWAYS)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        configureItems()
+        requireActivity().onBackPressedDispatcher.addCallback {
+            setToolbarScrollFlags(0)
+            requireActivity()
+                .findNavController(R.id.hostFragment)
+                .popBackStack()
+            return@addCallback true
+        }
     }
+
+    private fun setToolbarScrollFlags(flags: Int) {
+        val toolbar = requireActivity().toolbar
+        val params = toolbar.layoutParams as AppBarLayout.LayoutParams
+        params.scrollFlags = flags
+        toolbar.layoutParams = params
+    }
+
+    private fun configureItems() {
+        val layoutManager = LinearLayoutManager(requireContext())
+        val dataSource = dataSourceOf(
+            *(0..100).map { Item }.toTypedArray()
+        )
+
+        allItems.setup {
+            withLayoutManager(layoutManager)
+            withDataSource(dataSource)
+
+            withItem<Item>(R.layout.view_anchor_based_behavior_item) {
+                onBind(::VH) { _, _ -> }
+            }
+        }
+    }
+
+    class VH(itemView: View) : com.afollestad.recyclical.ViewHolder(itemView)
+
+    object Item
 }
