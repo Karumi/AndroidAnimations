a887cd2bc472bb0e1936e9b83053426f3a74a27e
Serchinastico <54cymru@gmail.com>
2019-07-05 00:35:37 +0200

    Add an exercise on motion layout with view pager

--- app/src/main/java/com/karumi/androidanimations/motionlayout/MotionLayoutFragment.kt
+++ app/src/main/java/com/karumi/androidanimations/motionlayout/MotionLayoutFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.karumi.androidanimations.R
 import kotlinx.android.synthetic.main.fragment_motion_layout.*
 
@@ -18,7 +20,80 @@ class MotionLayoutFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        container.transitionToEnd()
-        container.setDebugMode(MotionLayout.DEBUG_SHOW_PATH or MotionLayout.DEBUG_SHOW_PROGRESS)
+        val adapter = ViewPagerAdapter(requireFragmentManager())
+        adapter.addPage("BitterSweet", R.layout.motion_layout_view_pager_page1)
+        adapter.addPage("Java", R.layout.motion_layout_view_pager_page2)
+        adapter.addPage("Gulava", R.layout.motion_layout_view_pager_page3)
+        pager.adapter = adapter
+        tabLayout.setupWithViewPager(pager)
+        pager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                val numberOfPages = 3
+                container.progress = (position + positionOffset) / (numberOfPages - 1)
+            }
+
+            override fun onPageSelected(position: Int) {
+            }
+
+        })
+    }
+}
+
+class Page : Fragment() {
+
+    private var layoutId = 0
+
+    override fun setArguments(args: Bundle?) {
+        super.setArguments(args)
+        if (args != null) {
+            layoutId = args.getInt("layout")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(layoutId, container, false)
+    }
+}
+
+class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    private val fragmentList = ArrayList<Fragment>()
+    private val fragmentTitleList = ArrayList<String>()
+
+    override fun getItem(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        return fragmentList.size
+    }
+
+    private fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        fragmentTitleList.add(title)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return fragmentTitleList[position]
+    }
+
+    fun addPage(s: String, layout: Int) {
+        val page = Page()
+        val arg = Bundle()
+        arg.putInt("layout", layout)
+        page.arguments = arg
+        addFragment(page, s)
     }
 }
